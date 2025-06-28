e95328615aa64b506428afe088c31e09c03d7d65
peter reji paul <peterrejipaul@gmail.com>
2025-06-28 16:57:37 +0530

    Added Constructor

--- pl_java/exercise_1/CustomTime.java
+++ pl_java/exercise_1/CustomTime.java
@@ -5,7 +5,13 @@ public class CustomTime{
     int hour;   // current hour in military time
     int minute; // current minute in military time
     int second; // current second in military time
-  
+
+    // constructor to initialize the time to 00:00:00
+    CustomTime(){
+        hour = 0;
+        minute = 0;
+        second = 0;
+    }
     // return the time to the calling method in a three-membered array
     int[] getTime(){
         return new int[] {hour, minute, second};
